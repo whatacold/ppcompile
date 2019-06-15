32172db343dbe4945d3b54c6b3059809c1a95e41
whatacold <whatacold@gmail.com>
2019-06-15 18:00:55 +0800

    Ping and pong roughly works now.

--- ppcompile.el
+++ ppcompile.el
@@ -67,10 +67,7 @@
   "Arguments to `rsync' command to sync project to remote host."
   :group 'ppcompile)
 
-(defcustom ppcompile-rsync-src-dir nil
-  "Source directory to rsync files from."
-  :group 'ppcompile)
-
+;;; TODO could use mapping to deduce
 (defcustom ppcompile-rsync-dst-dir nil
   "Destination directory to rsync files into."
   :group 'ppcompile)
@@ -83,39 +80,9 @@
   "A list of cons'es tells how to map remote paths to local paths."
   :group 'ppcompile)
 
-(defconst ppcompile--with-password-path (concat (file-name-directory (buffer-file-name))
-                                                "with-password.exp"))
-
-(defun ppcompile--rsync ()
-  "Rsync files from local machine to remote one."
-  (let (rsync-args command full-dst)
-    (setq rsync-args "-avz") ; TODO add
-    (setq full-dst (format "%s@%s:%s"
-                           ppcompile-ssh-user
-                           ppcompile-ssh-host
-                           ppcompile-rsync-dst-dir))
-    (setq command (format "rsync -e 'ssh -p %d' %s %s %s"
-                          ppcompile-ssh-port
-                          rsync-args
-                          ppcompile-rsync-src-dir
-                          full-dst)) ; TODO rsync bin path
-    (shell-command-to-string command)))
-
-(defun ppcompile--compile ()
-  "Compile projects remotely and map paths in the output."
-  (let* (compile-command)
-    (save-some-buffers)
-    (add-to-list 'compilation-finish-functions #'ppcompile--convert-path) ; XXX how to achieve this elegantly?
-    (setq compile-command (format "expect %s %s ssh -p %d %s@%s %s" ; TODO add a variable for ssh path
-                                  ppcompile--with-password-path
-                                  ppcompile-ssh-password ; TODO hide in env
-                                  ppcompile-ssh-port
-                                  ppcompile-ssh-user
-                                  ppcompile-ssh-host
-                                  ppcompile-compile-command))
-    (compilation-start compile-command)
-                                        ;(compilation-start compile-command t) ; password may be needed, so comint mode
-    ))
+(defconst ppcompile--with-password-script-path (concat (file-name-directory (buffer-file-name))
+                                                       "with-password.exp")
+  "The path of the helper expect script, with-password.exp")
 
 (defun ppcompile--convert-path (buffer finish-msg)
   "Convert paths matching SRC to DST in current buffer."
@@ -125,6 +92,68 @@
         (goto-char (point-min))
         (while (search-forward (car map) nil t)
           (replace-match (cdr map)))))))
+
+(defun ppcompile--project-root ()
+  "Find the root directory of current prject.
+
+If `project-current' finds the root, return it;
+or else fallback to the `.git' directory."
+  (or (cdr (project-current))
+      (locate-dominating-file default-directory ".git")
+      default-directory)) ; TODO use lambda to exclude .git file
+
+(defun ppcompile-ping ()
+  "Rsync files from local machine to remote one."
+  (let* ((default-directory (ppcompile--project-root))
+         (process-environment (cons (format "PPCOMPILE_PASSWORD=%s"
+                                            ppcompile-ssh-password)
+                                    process-environment))
+         (process-connection-type nil)
+         rsync-args full-dst rc output)
+    (message "p e: %s" process-environment)
+    (setq rsync-args "-avz") ; TODO add
+    (setq full-dst (format "%s@%s:%s"
+                           ppcompile-ssh-user
+                           ppcompile-ssh-host
+                           ppcompile-rsync-dst-dir))
+    (with-temp-buffer
+      (setq rc (call-process "expect" nil (current-buffer) nil
+                             ppcompile--with-password-script-path
+                             "rsync"
+                             "-e"
+                             (format "ssh -p %d" ppcompile-ssh-port)
+                             rsync-args
+                             (expand-file-name default-directory)
+                             full-dst))
+      (setq output (buffer-substring-no-properties (point-min) (point-max))))
+    (cons rc output)))
+
+(defun ppcompile-pong ()
+  "Compile projects remotely and map paths in the output."
+  (let* ((default-directory (ppcompile--project-root))
+         (compilation-environment (cons (format "PPCOMPILE_PASSWORD=%s"
+                                                ppcompile-ssh-password)
+                                        compilation-environment))
+         compile-command)
+    (save-some-buffers)
+    (add-to-list 'compilation-finish-functions #'ppcompile--convert-path) ; XXX how to achieve this in an elegant way?
+    (setq compile-command (format "expect %s ssh -p %d %s@%s %s"
+                                  ppcompile--with-password-script-path
+                                  ppcompile-ssh-port
+                                  ppcompile-ssh-user
+                                  ppcompile-ssh-host
+                                  ppcompile-compile-command))
+    (compilation-start compile-command)
+                                        ;(compilation-start compile-command t) ; password may be needed, so comint mode
+    ))
+
+(defun ppcompile (&optional dont't-compile)
+  (interactive "P")
+  (let* ((rsync-result (ppcompile-ping)))
+    (if (not (eq 0 (car rsync-result)))
+        (message "Failed to rsync current project to the remote machine")
+      (unless don't-compile
+        (ppcompile-pong)))))
 
 (provide 'ppcompile)
 
