a188f7690f2e0c7f746db4404da5799975b5ce1c
Guangwang Huang <whatacold@gmail.com>
2022-06-03 09:36:03 +0800

    fix issues by package-lint

--- ppcompile.el
+++ ppcompile.el
@@ -4,7 +4,7 @@
 ;; Maintainer: Guangwang Huang
 ;; URL: https://github.com/whatacold/ppcompile
 ;; Version: 0.1
-;; Package-Requires: ((emacs "24"))
+;; Package-Requires: ((emacs "25.1"))
 ;; Homepage: https://github.com/whatacold/ppcompile
 ;; Keywords: tools
 
